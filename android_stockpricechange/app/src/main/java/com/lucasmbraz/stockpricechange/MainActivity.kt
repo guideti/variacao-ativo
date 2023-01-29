fe8b999153417b23a90b64897c40d2cc52d06fde
Lucas M. Braz <lmonteirobraz@gmail.com>
2023-01-29 16:29:50 -0300

    Add started android project

---
+++ android_stockpricechange/app/src/main/java/com/lucasmbraz/stockpricechange/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.lucasmbraz.stockpricechange
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.lucasmbraz.stockpricechange.ui.theme.StockPriceChangeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StockPriceChangeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    LaunchFlutterButton()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LaunchFlutterButton() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text("Launch Flutter Activity")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    StockPriceChangeTheme {
+        LaunchFlutterButton()
+    }
+}

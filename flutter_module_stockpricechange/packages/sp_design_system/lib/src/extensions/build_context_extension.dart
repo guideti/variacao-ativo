eb54986504bcd5b03c4ee10a8be50ed6745d332e
Lucas M. Braz <lmonteirobraz@gmail.com>
2023-02-04 11:15:07 -0300

    Fix method name of colors extension

--- flutter_module_stockpricechange/packages/sp_design_system/lib/src/extensions/build_context_extension.dart
+++ flutter_module_stockpricechange/packages/sp_design_system/lib/src/extensions/build_context_extension.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:sp_design_system/src/shared/sp_colors.dart';
 
 extension BuildContextExtension on BuildContext {
-  SpColors get fireColors => Theme.of(this).brightness == Brightness.light ? SpColors.light : SpColors.dark;
+  SpColors get spColors => Theme.of(this).brightness == Brightness.light ? SpColors.light : SpColors.dark;
 }

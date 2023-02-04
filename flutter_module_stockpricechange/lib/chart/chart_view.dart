2a75a32f929ce58b4f12c2cb4d527b1cec06b3f3
Lucas M. Braz <lmonteirobraz@gmail.com>
2023-02-04 11:17:14 -0300

    Show company name and ticker on ChartView

--- flutter_module_stockpricechange/lib/chart/chart_view.dart
+++ flutter_module_stockpricechange/lib/chart/chart_view.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:sp_design_system/sp_design_system.dart';
 
 class ChartView extends StatelessWidget {
   const ChartView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Chart'),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Column(
+                children: [
+                  SpText.header('SBUX', color: context.spColors.header),
+                  SpText.bodyRegular12('Starbucks', color: context.spColors.bodyLight),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }

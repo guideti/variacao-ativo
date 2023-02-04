1d1153b580c7816aafea9cf635a0f5a58a35e21a
Lucas M. Braz <lmonteirobraz@gmail.com>
2023-02-04 14:06:43 -0300

    First pass on showing historical data in a table

--- flutter_module_stockpricechange/lib/historical/stock_historical_view.dart
+++ flutter_module_stockpricechange/lib/historical/stock_historical_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_module_stockpricechange/historical/cubit/stock_historical_data_cubit.dart';
+import 'package:sp_design_system/sp_design_system.dart';
 
 import 'cubit/stock_historical_data_state.dart';
 
@@ -9,27 +10,46 @@ class StockHistoricalView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: BlocBuilder<StockHistoricalDataCubit, StockHistoricalDataState>(builder: (context, state) {
-        return state.when(
-          initial: () => Container(),
-          loading: () => const Text('Loading'),
-          success: (historicalItems) {
-            return ListView.builder(
-              itemCount: historicalItems.length,
-              itemBuilder: (context, index) {
-                final item = historicalItems[index];
-                return ListTile(
-                  title: Text("Price: R\$${item.openPrice.toStringAsFixed(2)}"),
-                  subtitle: Text(
-                      "D1: ${item.dayOneChange == null ? '-' : (item.dayOneChange! * 100).toStringAsFixed(2)} \t\t Previous: ${item.previousDayChange == null ? '-' : (item.previousDayChange! * 100).toStringAsFixed(2)}"),
-                );
-              },
-            );
-          },
-          failure: () => const Text('Failure'),
-        );
-      }),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Center(
+        child: BlocBuilder<StockHistoricalDataCubit, StockHistoricalDataState>(builder: (context, state) {
+          return state.when(
+            initial: () => Container(),
+            loading: () => const Text('Loading'),
+            success: (historicalItems) {
+              final widgets = historicalItems
+                  .map((e) => [
+                        SpText.bodyRegular14('01/01/2021', color: context.spColors.body),
+                        SpText.bodyRegular14('R\$${e.openPrice.toStringAsFixed(2)}', color: context.spColors.body),
+                        SpText.bodyRegular14(
+                          e.previousDayChange == null ? '-' : '${(e.previousDayChange! * 100).toStringAsFixed(2)}%',
+                          color: context.spColors.body,
+                        ),
+                        SpText.bodyRegular14(
+                          e.dayOneChange == null ? '-' : '${(e.dayOneChange! * 100).toStringAsFixed(2)}%',
+                          color: context.spColors.body,
+                        ),
+                      ])
+                  .toList()
+                  .expand((i) => i)
+                  .toList();
+
+              return GridView.count(
+                crossAxisCount: 4,
+                children: [
+                  SpText.bodyMedium14('Data', color: context.spColors.body),
+                  SpText.bodyMedium14('Valor', color: context.spColors.body),
+                  SpText.bodyMedium14('% D-1', color: context.spColors.body),
+                  SpText.bodyMedium14('% Total', color: context.spColors.body),
+                  ...widgets,
+                ],
+              );
+            },
+            failure: () => const Text('Failure'),
+          );
+        }),
+      ),
     );
   }
 }

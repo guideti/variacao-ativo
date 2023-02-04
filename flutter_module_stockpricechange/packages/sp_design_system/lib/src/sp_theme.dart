43de6e5c431fb7240084eb8611af6d9219c1aaa8
Lucas M. Braz <lmonteirobraz@gmail.com>
2023-02-04 10:43:01 -0300

    Set appBar style globally to match designs

--- flutter_module_stockpricechange/packages/sp_design_system/lib/src/sp_theme.dart
+++ flutter_module_stockpricechange/packages/sp_design_system/lib/src/sp_theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sp_design_system/src/shared/sp_colors.dart';
 
 class SpTheme {
@@ -6,9 +7,27 @@ class SpTheme {
 
   static final ThemeData light = ThemeData(
     scaffoldBackgroundColor: SpColors.light.background,
+    appBarTheme: const AppBarTheme(
+      elevation: 0,
+      color: Colors.transparent,
+      iconTheme: IconThemeData(color: Colors.black),
+      foregroundColor: Colors.black,
+      systemOverlayStyle: SystemUiOverlayStyle(
+        statusBarBrightness: Brightness.light,
+      ),
+    ),
   );
 
   static final ThemeData dark = ThemeData.dark().copyWith(
     scaffoldBackgroundColor: SpColors.dark.background,
+    appBarTheme: const AppBarTheme(
+      elevation: 0,
+      color: Colors.transparent,
+      iconTheme: IconThemeData(color: Colors.white),
+      foregroundColor: Colors.white,
+      systemOverlayStyle: SystemUiOverlayStyle(
+        statusBarBrightness: Brightness.dark,
+      ),
+    ),
   );
 }

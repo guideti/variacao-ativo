23b62d4d2173133b35b304748b5b564bc420691b
Lucas M. Braz <lmonteirobraz@gmail.com>
2023-02-04 11:09:12 -0300

    Define text colors for header, body and bodyLight

--- flutter_module_stockpricechange/packages/sp_design_system/lib/src/shared/sp_colors.dart
+++ flutter_module_stockpricechange/packages/sp_design_system/lib/src/shared/sp_colors.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
 
 class SpColors {
-  SpColors._({required this.background});
+  SpColors._({
+    required this.background,
+    required this.header,
+    required this.body,
+    required this.bodyLight,
+  });
 
   static SpColors light = SpColors._(
-    background: SpColor._(0xFFFFFFFF),
+    background: const SpColor._(0xFFFFFFFF),
+    header: const SpColor._(0xFF000000),
+    body: const SpColor._(0xFF000000),
+    bodyLight: const SpColor._(0xFF5F676F),
   );
 
   static SpColors dark = SpColors._(
-    background: SpColor._(0xFF1B1B1B),
+    background: const SpColor._(0xFF1B1B1B),
+    header: const SpColor._(0xFFFFFFFF),
+    body: const SpColor._(0xFFF5F5F5),
+    bodyLight: const SpColor._(0xFF9AA4AE),
   );
 
   final SpColor background;
+  final SpColor header;
+  final SpColor body;
+  final SpColor bodyLight;
 }
 
 class SpColor extends Color {
-  SpColor._(super.value);
+  const SpColor._(super.value);
 }

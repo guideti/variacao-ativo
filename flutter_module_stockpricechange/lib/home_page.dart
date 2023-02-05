4cddcb7ceafb068f73f28cca1255fd00be1ee1ec
Lucas M. Braz <lmonteirobraz@gmail.com>
2023-02-04 22:51:40 -0300

    Remove bloc/cubit from the app

--- flutter_module_stockpricechange/lib/home_page.dart
+++ flutter_module_stockpricechange/lib/home_page.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_module_stockpricechange/chart/chart_view.dart';
-import 'package:flutter_module_stockpricechange/historical/cubit/stock_historical_data_cubit.dart';
 import 'package:flutter_module_stockpricechange/pigeon.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:stock_repository/stock_repository.dart';
 
 import 'historical/stock_historical_view.dart';
 
@@ -45,23 +42,20 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => StockHistoricalDataCubit(StockRepository())..fetchData(),
-      child: Scaffold(
-        appBar: AppBar(
-          leading: Padding(
-            padding: const EdgeInsets.only(left: 8.0),
-            child: IconButton(
-              // TODO(lucas): Use constants for assets
-              icon: SvgPicture.asset('assets/back_icon.svg'),
-              onPressed: () => navigationApi.goBack(),
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: IconButton(
+            // TODO(lucas): Use constants for assets
+            icon: SvgPicture.asset('assets/back_icon.svg'),
+            onPressed: () => navigationApi.goBack(),
           ),
-          title: _visualisationType == VisualisationType.historical ? const Text('Variação do Ativo') : null,
         ),
-        body: SafeArea(
-          child: _visualisationType == VisualisationType.historical ? const StockHistoricalView() : const ChartView(),
-        ),
+        title: _visualisationType == VisualisationType.historical ? const Text('Variação do Ativo') : null,
+      ),
+      body: SafeArea(
+        child: _visualisationType == VisualisationType.historical ? const StockHistoricalView() : const ChartView(),
       ),
     );
   }

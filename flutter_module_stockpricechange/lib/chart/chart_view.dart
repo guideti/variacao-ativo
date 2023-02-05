252e3eb0e40bf9634a9dbbaf641fba4042dd94a5
Lucas M. Braz <lmonteirobraz@gmail.com>
2023-02-04 22:46:04 -0300

    First pass on connecting ChartView with redux

--- flutter_module_stockpricechange/lib/chart/chart_view.dart
+++ flutter_module_stockpricechange/lib/chart/chart_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_module_stockpricechange/chart/chart.dart';
+import 'package:flutter_module_stockpricechange/redux/app_state.dart';
 import 'package:flutter_module_stockpricechange/stock_info.dart';
+import 'package:flutter_module_stockpricechange/widgets/error_with_retry.dart';
+import 'package:flutter_module_stockpricechange/widgets/loading.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:sp_design_system/sp_design_system.dart';
 
@@ -9,45 +13,56 @@ class ChartView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const StockInfo(),
-          const SizedBox(height: 20),
-          const AspectRatio(
-            aspectRatio: 2,
-            child: Chart(),
-          ),
-          const SizedBox(height: 28),
-          SpText.bodyMedium14('Statistics', color: context.spColors.body),
-          const SizedBox(height: 16),
-          const _StatRow(title: 'Previous Close', value: '\$1,800'),
-          const SizedBox(height: 16),
-          const _Divider(),
-          const SizedBox(height: 16),
-          const _StatRow(title: 'Opening Price', value: '\$1,860'),
-          const SizedBox(height: 16),
-          const _Divider(),
-          const SizedBox(height: 16),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return StoreConnector<AppState, AppState>(
+      converter: (store) => store.state,
+      builder: (context, state) {
+        if (state.failed || state.tradingDays.isEmpty) {
+          return const ErrorWithRetry();
+        }
+        if (state.isLoading) {
+          return const Loading();
+        }
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SpText.bodyRegular14('24H Returns %', color: context.spColors.bodyLight),
-              // TODO(lucas): Use widget for variable percentage
+              const StockInfo(),
+              const SizedBox(height: 20),
+              const AspectRatio(
+                aspectRatio: 2,
+                child: Chart(),
+              ),
+              const SizedBox(height: 28),
+              SpText.bodyMedium14('Statistics', color: context.spColors.body),
+              const SizedBox(height: 16),
+              const _StatRow(title: 'Previous Close', value: '\$1,800'),
+              const SizedBox(height: 16),
+              const _Divider(),
+              const SizedBox(height: 16),
+              const _StatRow(title: 'Opening Price', value: '\$1,860'),
+              const SizedBox(height: 16),
+              const _Divider(),
+              const SizedBox(height: 16),
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  // TODO(lucas): Use constant for assets
-                  SvgPicture.asset('assets/arrow_up.svg'),
-                  SpText.bodyRegular12('2.35%', color: SpColors.green),
+                  SpText.bodyRegular14('24H Returns %', color: context.spColors.bodyLight),
+                  // TODO(lucas): Use widget for variable percentage
+                  Row(
+                    children: [
+                      // TODO(lucas): Use constant for assets
+                      SvgPicture.asset('assets/arrow_up.svg'),
+                      SpText.bodyRegular12('2.35%', color: SpColors.green),
+                    ],
+                  )
                 ],
-              )
+              ),
+              const SizedBox(height: 16),
             ],
           ),
-          const SizedBox(height: 16),
-        ],
-      ),
+        );
+      },
     );
   }
 }

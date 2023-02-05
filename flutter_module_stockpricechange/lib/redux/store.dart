bfcf5fa584a89298b0c75b322614bcd63bcc803a
Lucas M. Braz <lmonteirobraz@gmail.com>
2023-02-05 12:20:03 -0300

    Remove unused code

--- flutter_module_stockpricechange/lib/redux/store.dart
+++ flutter_module_stockpricechange/lib/redux/store.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_module_stockpricechange/models/trading_day.dart';
 import 'package:flutter_module_stockpricechange/redux/app_state.dart';
 import 'package:flutter_module_stockpricechange/redux/reducers.dart';
 import 'package:flutter_module_stockpricechange/redux/repository_middleware.dart';
@@ -8,24 +7,7 @@ import 'package:stock_repository/stock_repository.dart';
 Store<AppState> createReduxStore() => Store<AppState>(
       appReducer,
       initialState: AppState(
-        tradingDays: [
-          TradingDay(
-            day: DateTime(2023, 2),
-            openPrice: 25.5,
-          ),
-          TradingDay(
-            day: DateTime(2023, 2, 2),
-            openPrice: 25.5,
-          ),
-          TradingDay(
-            day: DateTime(2023, 2, 3),
-            openPrice: 25.5,
-          ),
-          TradingDay(
-            day: DateTime(2023, 2, 4),
-            openPrice: 25.5,
-          ),
-        ],
+        tradingDays: const [],
         isLoading: false,
         failed: false,
       ),

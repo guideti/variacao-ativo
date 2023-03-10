f9dd70c9296345b6eb4e4570a26a9c3ca9127d7c
Master <leandro.dev.dotnet@gmail.com>
2023-03-10 15:25:04 -0300

    projeto variacao ativo

---
+++ src/Variacao.Ativo.Domain/Entities/ConsultAtivo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Variacao.Ativo.Domain.Entities
+{
+    [Table("Ativo")]
+    public class ConsultAtivo : BaseEntity
+    {
+        public string Data { get; set; }
+        public decimal Valor { get; set; }
+        public string? VariacaoDiaMenos1 { get; set; }
+        public string? VariacaoPrimeiraData { get; set; }
+    }
+}

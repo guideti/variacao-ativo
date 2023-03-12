3c65ae2ef67ab71343770971973f3c349783b1b5
RaulRodrigopopcode <78648287+RaulRodrigo06@users.noreply.github.com>
2023-03-12 16:42:16 -0300

    feat: creating ios project

---
+++ variacao-ativa/variacao-ativa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  variacao-ativa
+//
+//  Created by Raul Rodrigo on 12/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ variacao-ativa/variacao-ativaUITests/variacao_ativaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  variacao_ativaUITestsLaunchTests.swift
+//  variacao-ativaUITests
+//
+//  Created by Raul Rodrigo on 12/03/23.
+//
+
+import XCTest
+
+class variacao_ativaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
